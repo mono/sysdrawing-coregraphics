d66db72893f311ab5877284b3a5e7453c6d851d5
kjpou1 <kjpou@pt.lu>
2012-12-30 09:00:02 +0100

    Add example for 3D spheres.

---
+++ Samples/MonoMac/Example5_6/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+
+namespace Example5_6
+{
+	// Should subclass MonoMac.AppKit.NSResponder
+	[MonoMac.Foundation.Register("AppDelegate")]
+	public partial class AppDelegate
+	{
+	}
+}
+
---
+++ Samples/MonoMac/Example5_6/Point3.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example5_6
+{
+	public class Point3
+	{
+		public float X;
+		public float Y;
+		public float Z;
+		public float W = 1f;
+
+		public Point3 ()
+		{
+		}
+
+		public Point3 (float x, float y, float z, float w)
+		{
+			X = x;
+			Y = y;
+			Z = z;
+			W = w;
+		}
+
+		// Apply a transformation to a point:
+		public void Transform (Matrix3 m)
+		{
+			float[] result = m.VectorMultiply (new float[4] { X, Y, Z, W });
+			X = result [0];
+			Y = result [1];
+			Z = result [2];
+			W = result [3];
+		}
+
+		public void TransformNormalize (Matrix3 m)
+		{
+			float[] result = m.VectorMultiply (new float[4] { X, Y, Z, W });
+			X = result [0] / result [3];
+			Y = result [1] / result [3];
+			Z = result [2];
+			W = 1;
+		}
+	}
+}
