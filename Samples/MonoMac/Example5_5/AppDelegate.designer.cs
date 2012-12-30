135fa98d1649d85735921620b0883f573926869b
kjpou1 <kjpou@pt.lu>
2012-12-30 08:45:41 +0100

    Add new example testing sub views and drawing a cylinder.

---
+++ Samples/MonoMac/Example5_5/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+
+namespace Example5_5
+{
+	// Should subclass MonoMac.AppKit.NSResponder
+	[MonoMac.Foundation.Register("AppDelegate")]
+	public partial class AppDelegate
+	{
+	}
+}
+
---
+++ Samples/MonoTouch/MTExample5_5/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+
+namespace MTExample5_5
+{
+	// Should subclass MonoMac.AppKit.NSResponder
+	[MonoMac.Foundation.Register("AppDelegate")]
+	public partial class AppDelegate
+	{
+	}
+}
+
