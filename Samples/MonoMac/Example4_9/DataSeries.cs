43b89c56e14da31a280ebe7e59d1ca0f21863920
kjpou1 <kjpou@pt.lu>
2012-12-30 07:41:45 +0100

    Add examples MTExample4_9 and Example4_9 for reading text file and displaying Stock Chart.

---
+++ Samples/MonoMac/Example4_9/DataSeries.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Drawing;
+
+namespace Example4_9
+{
+    public class DataSeries
+    {
+        private string[,] dataString;
+        private LineStyle lineStyle;
+        private string seriesName = "";
+
+        public DataSeries()
+        {
+            lineStyle = new LineStyle();
+        }
+
+        public LineStyle LineStyle
+        {
+            get { return lineStyle; }
+            set { lineStyle = value; }
+        }
+
+        public string[,] DataString
+        {
+            get { return dataString; }
+            set { dataString = value; }
+        }
+
+        public string SeriesName
+        {
+            get { return seriesName; }
+            set { seriesName = value; }
+        }
+    }
+}
+
+
