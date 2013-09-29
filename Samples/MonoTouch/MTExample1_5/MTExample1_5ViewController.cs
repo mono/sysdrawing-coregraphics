ebd31a478dcbff02f966ca6cb4c5137948a09a7f
kjpou1 <kjpou@pt.lu>
2013-09-29 09:02:58 +0200

    Change to Universal and upgrade to iOS7.

--- Samples/MonoTouch/MTExample1_5/MTExample1_5ViewController.cs
+++ Samples/MonoTouch/MTExample1_5/MTExample1_5ViewController.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Drawing;
-
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
@@ -8,10 +7,15 @@ namespace MTExample1_5
 {
 	public partial class MTExample1_5ViewController : UIViewController
 	{
-		public MTExample1_5ViewController () : base ("MTExample1_5ViewController", null)
+		static bool UserInterfaceIdiomIsPhone {
+			get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+		}
+
+		public MTExample1_5ViewController ()
+			: base (UserInterfaceIdiomIsPhone ? "MTExample1_5ViewController_iPhone" : "MTExample1_5ViewController_iPad", null)
 		{
 		}
-		
+
 		public override void DidReceiveMemoryWarning ()
 		{
 			// Releases the view if it doesn't have a superview.
@@ -19,30 +23,22 @@ namespace MTExample1_5
 			
 			// Release any cached data, images, etc that aren't in use.
 		}
-		
+
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
 			
 			// Perform any additional setup after loading the view, typically from a nib.
 		}
-		
-		public override void ViewDidUnload ()
-		{
-			base.ViewDidUnload ();
-			
-			// Clear any references to subviews of the main view in order to
-			// allow the Garbage Collector to collect them sooner.
-			//
-			// e.g. myOutlet.Dispose (); myOutlet = null;
-			
-			ReleaseDesignerOutlets ();
-		}
-		
+
 		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
 		{
 			// Return true for supported orientations
-			return true;
+			if (UserInterfaceIdiomIsPhone) {
+				return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+			} else {
+				return true;
+			}
 		}
 	}
 }
