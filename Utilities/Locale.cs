276fbe4fccd2c36747ef70423a6a5db15e54aa78
Can Erten <can@codingday.com>
2014-01-11 08:22:40 +0000

    forgotten locale conversion

--- Utilities/Locale.cs
+++ Utilities/Locale.cs
@@ -1,7 +1,10 @@
 using System;
 
-namespace System.DrawingNative
-{
+#if MONOMAC
+namespace System.DrawingNative  {
+#else
+namespace System.Drawing {
+	#endif
 	public class Locale
 	{
 		public static string GetText (string format)
