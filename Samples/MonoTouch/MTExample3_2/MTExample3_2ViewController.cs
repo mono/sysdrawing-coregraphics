9d7d7f57816239f23234cf26c1a6f8031287e6d7
olegoid <olegoid91@gmail.com>
2015-12-23 16:34:41 -0300

    [MTExample3_2] Migrate sample to unified

--- Samples/MonoTouch/MTExample3_2/MTExample3_2ViewController.cs
+++ Samples/MonoTouch/MTExample3_2/MTExample3_2ViewController.cs
@@ -1,44 +1,19 @@
-using System;
-using System.Drawing;
-using Foundation;
 using UIKit;
 
-namespace MTExample3_2
-{
-	public partial class MTExample3_2ViewController : UIViewController
-	{
+namespace MTExample3_2 {
+	public partial class MTExample3_2ViewController : UIViewController {
 		static bool UserInterfaceIdiomIsPhone {
 			get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
 		}
 
-		public MTExample3_2ViewController ()
-			: base (UserInterfaceIdiomIsPhone ? "MTExample3_2ViewController_iPhone" : "MTExample3_2ViewController_iPad", null)
+		public MTExample3_2ViewController()
+			: base(UserInterfaceIdiomIsPhone ? "MTExample3_2ViewController_iPhone" : "MTExample3_2ViewController_iPad", null)
 		{
-		}
-
-		public override void DidReceiveMemoryWarning ()
-		{
-			// Releases the view if it doesn't have a superview.
-			base.DidReceiveMemoryWarning ();
-			
-			// Release any cached data, images, etc that aren't in use.
-		}
-
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
-			
-			// Perform any additional setup after loading the view, typically from a nib.
 		}
 
 		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
 		{
-			// Return true for supported orientations
-			if (UserInterfaceIdiomIsPhone) {
-				return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
-			} else {
-				return true;
-			}
+			return !UserInterfaceIdiomIsPhone || (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
 		}
 	}
 }
