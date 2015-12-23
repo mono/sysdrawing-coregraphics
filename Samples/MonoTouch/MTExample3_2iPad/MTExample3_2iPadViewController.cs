2f1041241f29eb13c58d6860e0bb4be608313702
olegoid <olegoid91@gmail.com>
2015-12-23 17:01:01 -0300

    [MTExample3_2iPad] Migrate sample to unified

--- Samples/MonoTouch/MTExample3_2iPad/MTExample3_2iPadViewController.cs
+++ Samples/MonoTouch/MTExample3_2iPad/MTExample3_2iPadViewController.cs
@@ -1,47 +1,19 @@
-using System;
-using System.Drawing;
-
-using Foundation;
 using UIKit;
 
-namespace MTExample3_2
-{
-	public partial class MTExample3_2iPadViewController : UIViewController
-	{
+namespace MTExample3_2 {
+	public partial class MTExample3_2iPadViewController : UIViewController {
 		public MTExample3_2iPadViewController () : base ("MTExample3_2iPadViewController", null)
 		{
-		}
-		
-		public override void DidReceiveMemoryWarning ()
-		{
-			// Releases the view if it doesn't have a superview.
-			base.DidReceiveMemoryWarning ();
-			
-			// Release any cached data, images, etc that aren't in use.
-		}
-		
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
-			
-			// Perform any additional setup after loading the view, typically from a nib.
 		}
 		
 		public override void ViewDidUnload ()
 		{
 			base.ViewDidUnload ();
-			
-			// Clear any references to subviews of the main view in order to
-			// allow the Garbage Collector to collect them sooner.
-			//
-			// e.g. myOutlet.Dispose (); myOutlet = null;
-			
 			ReleaseDesignerOutlets ();
 		}
 		
 		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
 		{
-			// Return true for supported orientations
 			return true;
 		}
 	}
