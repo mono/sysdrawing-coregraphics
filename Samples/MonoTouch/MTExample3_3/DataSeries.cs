de0e630b55ed9a8051968e9bb25023290e47f42e
olegoid <olegoid91@gmail.com>
2015-12-23 20:33:50 -0300

    [MTExample3_3] Migrate sample to unified

--- Samples/MonoTouch/MTExample3_3/DataSeries.cs
+++ Samples/MonoTouch/MTExample3_3/DataSeries.cs
@@ -1,44 +1,50 @@
-using System;
 using System.Collections;
-using System.Drawing;
+using CoreGraphics;
 
-namespace MTExample3_3
-{
-    public class DataSeries
-    {
-        private ArrayList pointList;
-        private LineStyle lineStyle;
-        private string seriesName = "Default Name";
+namespace MTExample3_3 {
+	public class DataSeries {
+		ArrayList pointList;
+		LineStyle lineStyle;
+		string seriesName = "Default Name";
 
-        public DataSeries()
-        {
-            lineStyle = new LineStyle();
-            pointList = new ArrayList();
-        }
+		public DataSeries ()
+		{
+			lineStyle = new LineStyle ();
+			pointList = new ArrayList ();
+		}
 
-        public LineStyle LineStyle
-        {
-            get { return lineStyle; }
-            set { lineStyle = value; }
-        }
+		public LineStyle LineStyle {
+			get {
+				return lineStyle;
+			}
+			set {
+				lineStyle = value;
+			}
+		}
 
-        public ArrayList PointList
-        {
-            get { return pointList; }
-            set { pointList = value; }
-        }
+		public ArrayList PointList {
+			get {
+				return pointList;
+			}
+			set {
+				pointList = value;
+			}
+		}
 
-        public void AddPoint(PointF pt)
-        {
-            pointList.Add(pt);
-        }
+		public void AddPoint (CGPoint pt)
+		{
+			pointList.Add (pt);
+		}
 
-        public string SeriesName
-        {
-            get { return seriesName; }
-            set { seriesName = value; }
-        }
-    }
+		public string SeriesName {
+			get {
+				return seriesName;
+			}
+			set {
+				seriesName = value;
+			}
+		}
+	}
 }
 
 
